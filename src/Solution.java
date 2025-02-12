0cce7f39602bda3c26c3b3d78700f3798c1fbc32
Dalton Wiggins <wiggins.dalton.t@gmail.com>
2025-02-12 14:22:25 -0800

    Trees - Created TreeMap

--- src/Solution.java
+++ src/Solution.java
@@ -2,30 +2,5 @@ import java.util.*;
 
 @SuppressWarnings("unused")
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        if(temperatures.length == 1){
-            return new int[] {0};
-        }
-
-        int[] res = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>();
-
-        for(int i = 0; i < temperatures.length; i++){
-            while(!stack.isEmpty() && temperatures[i] > stack.peek()[0]){
-                int[] tmp = stack.pop();
-                res[tmp[1]] = i - tmp[1];
-            }
-
-            stack.add(new int[] {temperatures[i], i});
-        }
-
-
-
-        return res;
-    }
+    
 }
-/*
-temperatures[] is an array of the temperature on the ith day
-return res[], where res[i], is the number of days until a warmer day will come
-
-*/
