94194e4841145cfac5d742468a5a45c8643d2c9a
Dalton Wiggins <wiggins.dalton.t@gmail.com>
2025-03-05 22:10:10 -0800

    Spiral Traverse - In Progress

--- src/Solution.java
+++ src/Solution.java
@@ -8,5 +8,18 @@ import java.util.*;
 
 @SuppressWarnings("unused")
 class Solution {
-    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[1].length;
+        List<Integer> res = new ArrayList<>(rows * cols);
+
+        boolean atMaxR = false, atMaxC = false;
+        int minR = 0, maxR = rows, minC = 0, maxC = cols, r = 0, c = 0;
+        for(int val : res){
+            val = matrix[r][c];
+
+
+        }
+        
+        return res;
+    }
 }
