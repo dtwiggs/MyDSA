e3c38bf606a3ccc3894e76b650faace9e24ab403
dtwiggs <wiggins.dalton.t@gmail.com>
2025-12-04 14:11:27 -0800

    Linked List - Middle Node Fast and Slow Pointer

---
+++ src/LinkedList/MiddleNode.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
