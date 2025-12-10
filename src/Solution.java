a6448dcdb906e74bacb1b75652cecf64c78243fa
dtwiggs <wiggins.dalton.t@gmail.com>
2025-12-10 15:35:44 -0800

    Solution in progress...

--- src/Solution.java
+++ src/Solution.java
@@ -7,5 +7,22 @@ import java.util.*;
 @SuppressWarnings("unused")
 
 class Solution {
-    
+    public int pairSum(ListNode head) {
+        int res = 0;
+        ListNode fast = head, slow = head;
+
+        while(fast != null || fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = slow;
+        while(slow != null){
+            slow = slow.next;
+            prev.next = 
+            prev = slow;
+        }
+
+        return res;
+    }
 }
