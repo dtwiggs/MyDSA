71cc4906122c5e0be5c572542a63228740ee555c
dtwiggs <wiggins.dalton.t@gmail.com>
2025-12-16 15:47:49 -0800

    Duplicate in progress

--- src/Solution.java
+++ src/Solution.java
@@ -9,16 +9,5 @@ import LinkedList.ListNode;
 @SuppressWarnings("unused")
 
 class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if(head == null) return head;
-        ListNode fast = head, slow = head;
-
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(slow == fast) return slow;;
-        }
-
-        return fast;
-    }
+    
 }
