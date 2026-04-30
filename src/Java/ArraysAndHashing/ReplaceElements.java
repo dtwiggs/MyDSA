7e33ba96aa76649f9b2a23697805fab8a6b4af7f
dtwiggs <wiggins.dalton.t@gmail.com>
2026-04-30 08:32:45 -0700

    Java Arrays and Hashing -  Replace Elements

---
+++ src/Java/ArraysAndHashing/ReplaceElements.java
@@ -0,0 +1,16 @@
+package Java.ArraysAndHashing;
+
+public class ReplaceElements {
+    public int[] replaceElements(int[] arr){
+        int size = arr.length;
+        int max = -1;
+        int[] res = new int[size];
+
+        for(int i = size - 1; i > -1; i--){
+            res[i] = max;
+            max = Math.max(max, arr[i]);
+        }
+
+        return res;
+    }
+}
--- src/Python/ArraysAndHashing/replace_elements.py
+++ src/Python/ArraysAndHashing/replace_elements.py
@@ -4,11 +4,10 @@ class replaceElements:
     def replace_elements(self, arr: List[int]) -> List[int]:
         size = len(arr)
         res = [0] * size
-        res[size - 1] = -1
-        max_num = arr[size - 1]
+        max_num = -1
 
-        for i in range(size - 1, 0, -1):
+        for i in range(size - 1, -1, -1):
+            res[i] = max_num
             max_num = max(arr[i], max_num)
-            res[i - 1] = max(arr[i], max_num)
             
         return res
