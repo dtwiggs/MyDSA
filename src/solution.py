8b539660f6c777cd5935202cb12de4219046bbbf
dtwiggs <wiggins.dalton.t@gmail.com>
2026-05-11 15:59:40 -0700

    Browser History

--- src/solution.py
+++ src/solution.py
@@ -4,16 +4,31 @@
 from typing import List, Optional
 
 class ListNode:
-    def __init__(self, val = 0, next = None):
-        self.val = val
+    def __init__(self, page: str, next = None, prev = None):
+        self.page = page
         self.next = next
+        self.prev = prev
 
 class Solution:
-    def reverse_list(self, head: ListNode | None) -> ListNode | None:
-        if head:
-            self.reverse_list_rec(head)
-        return head
+    home, curr = None, None
+    def __init__(self, homepage: str):
+        self.homepage = homepage
+        home = ListNode(homepage)
+        curr = home
 
-    def reverse_list_rec(self, node: ListNode) -> ListNode:
+    def visit(self, url: str) -> None:
+        curr.next = ListNode(url)
+        curr.next.prev = curr
+        curr = curr.next
 
-        return node
+    def back(self, steps: int) -> str:
+        while curr.prev and steps > 0:
+            curr = curr.prev
+            steps -= 1
+        return curr.page
+
+    def forward(self, steps: int) -> str:
+        while curr.next and steps > 0:
+            curr = curr.next
+            steps -= 1
+        return curr.page
